7fbc2da42b44a2a07a91648025572bceb17b1dd4
Judy Lyn Medalla <judy.medalla@tuitt.com>
2022-07-27 10:50:09 +0800

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atl
 const app = express();
 
 const corsOptions = {
-	origin: ['http://localhost:3000', 'https://agitated-goldwasser-4eebd5.netlify.app'],
+	origin: ['http://localhost:3000', 'https://agitated-goldwasser-4eebd5.netlify.app', 'https://zuitt-shop-eta.vercel.app/'],
 	optionsSuccessStatus: 200
 }
 
